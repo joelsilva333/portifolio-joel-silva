8e3c9186944d8d008eac66482f43e1086091f309
joelsilva333 <joelpitra44@gmail.com>
2024-09-26 15:10:41 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/portifolio-joel-silva" element={<Home />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</Router>
