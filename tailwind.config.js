69cec763416b8cac685290d50c45bd417eb6ea48
Joel Silva <joelpitra44@gmail.com>
2025-06-16 13:58:18 +0100

    Formata o arquivo tailwind.config.js para melhorar a legibilidade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				Montserrat: ["Montserrat"],
-				Inconsolata: ["Inconsolata", "monospace"],
-			},
-			textColor: { whiteDefault: "#DEE5EF", blueDefault: "#0F172A", blueSecondary: '#223975' },
-			backgroundColor: { whiteDefault: "#DEE5EF", blueDefault: "#0F172A", blueSecondary: '#223975' },
-		},
-	},
-	plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Montserrat: ["Montserrat"],
+        Inconsolata: ["Inconsolata", "monospace"],
+      },
+      textColor: {
+        whiteDefault: "#DEE5EF",
+        blueDefault: "#0F172A",
+        blueSecondary: "#223975",
+      },
+      backgroundColor: {
+        whiteDefault: "#DEE5EF",
+        blueDefault: "#0F172A",
+        blueSecondary: "#223975",
+      },
+    },
+  },
+  plugins: [],
+};
