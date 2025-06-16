68d70cd058c2a25c6c8e5158876aba40d576ce60
Joel Silva <joelpitra44@gmail.com>
2025-06-16 11:00:26 +0100

    Formata o código no componente Home para melhorar a legibilidade

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,21 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import AboutMe from "../containers/home/AboutMe"
-import Banner from "../containers/home/Banner"
-import Enterprises from "../containers/home/Enterprises"
-import Projects from "../containers/home/Projects"
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import AboutMe from "../containers/home/AboutMe";
+import Banner from "../containers/home/Banner";
+import Enterprises from "../containers/home/Enterprises";
+import Projects from "../containers/home/Projects";
 
 const Home: React.FC = () => {
-	return (
-		<main className="bg-[#0F172A] text-[#DEE5EF] h-screen overflow-y-auto font-Montserrat scrollbar-custom max-lg:gap-10 gap-24 flex flex-col relative">
-			<Header />
-			<Banner />
-			<Projects />
-			<AboutMe />
-			<Enterprises />
-			<Footer />
-		</main>
-	)
-}
+  return (
+    <main className="bg-[#0F172A] text-[#DEE5EF] h-screen overflow-y-auto font-Montserrat scrollbar-custom max-lg:gap-10 gap-24 flex flex-col relative">
+      <Header />
+      <Banner />
+      <Projects />
+      <AboutMe />
+      <Enterprises />
+      <Footer />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
